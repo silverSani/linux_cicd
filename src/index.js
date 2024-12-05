66f5dc47d7b49e1402a69653cbd061b8c4d1cb3b
silverSani <eunsan12345@naver.com>
2024-12-05 19:57:23 +0900

    Chore: CD

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) =>{
-    res.send("<h1> Incheon National University Linux CI_CD! </h1>");
+    res.send("<h1> 종강 축하드립니다 !-! 감사했어요오 <3 </h1>");
 });
 
 app.listen(port, ()=>{
